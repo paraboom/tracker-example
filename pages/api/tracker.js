72b1bdaf7fa033d6e2848a31e89b506072f776ca
Ivan Shornikov <paraboom@gmail.com>
2022-04-10 18:58:26 +0300

    Fix scripts

--- pages/api/tracker.js
+++ pages/api/tracker.js
@@ -1,7 +1,7 @@
 const otherScript = `
     var script = document.createElement('script');
     script.async = true;
-    script.src = "http://localhost:3002/api/other";
+    script.src = "http://localhost:3001/api/other";
     document.head.appendChild(script);
     script.onload = () => {
         alert("Here we can use variables set by other scripts" + window.otherObject);
@@ -10,7 +10,7 @@ const otherScript = `
 
 const siteDependentScripts = {
     'ASDF-FDSA': [
-        otherScript
+        otherScript,
     ]
 }
 
@@ -26,11 +26,60 @@ const commonTracketScript = `
     });
 `
 
+
+const scriptTemplates = {
+    piwik: data => {
+        if (data.state !== 'enabled') return '';
+
+        return `
+            var script = document.createElement('script');
+            script.async = true;
+            script.src = "piwikserver" + ${data.PIWIKsiteId}; //env depended
+            document.head.appendChild(script);
+        `
+    },
+    toplist: data => {
+        if (data.state !== 'enabled') return '';
+
+        return `
+            var div = document.findElementByID('${data.HTMLContainerID}');
+            div.addAttribute('data-id', ${data.topListWidgetID});
+        
+            var script = document.createElement('script');
+            script.async = true;
+            script.src = "toplist"; //env depended
+            document.head.appendChild(script);
+        `
+    },
+}
+
 export default function handler(req, res) {
     const { siteId } = req.query;
 
     let resultScript = [commonTracketScript];
+
+    // fetch site settings
     const siteSpecificScripts = siteDependentScripts[siteId];
+
+
+    // const siteSpecificScripts = [
+    //     {
+    //         type: 'piwik',
+    //         params: {
+    //             state: 'enabled',
+    //             PIWIKsiteId: '34534'
+    //         }
+    //     },
+    //     {
+    //         type: 'toplist',
+    //         params: {
+    //             HTMLContainerID: 'toplist-contatiner',
+    //             topListWidgetID: '5464564'
+    //         }
+    //     },
+    // ]
+
+
     resultScript = resultScript.concat(siteSpecificScripts).join('\n\n');
 
     res
